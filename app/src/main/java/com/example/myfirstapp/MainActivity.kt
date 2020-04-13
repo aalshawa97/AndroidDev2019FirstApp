8ed77672e24902c02c441482dc7ce71c4b42a60a
aalshawa97 <abdullahalshawa@gmail.com>
2020-04-13 10:19:39 -0700

    Time out
    For the main activity.

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -8,12 +8,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val SPALSH_TIME_OUT = 4000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        /*new Handler().postDelay(new Runnable()){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+        */
         monster01.setOnClickListener{viewMonsterImage(R.drawable.monster01)}
         monster02.setOnClickListener{viewMonsterImage(R.drawable.monster02)}
         monster03.setOnClickListener{viewMonsterImage(R.drawable.monster03)}
